cd4d9bd412812204829888d40a8ea5c8cbaabdba
Harry Schwartz <harry@thoughtbot.com>
2014-06-19 12:42:34 -0400

    initial commit

---
+++ graph.rb
@@ -0,0 +1,47 @@
+class Graph
+  attr_reader :edges, :nodes
+
+  def initialize(edges, nodes = [])
+    @edges = edges
+    @nodes = (nodes + edges.flatten).uniq
+  end
+
+  def topological_sort
+    if nodes.empty?
+      []
+    else
+      start_nodes = nodes.select { |n| incoming(n).empty? }
+      removed_edges = start_nodes.map { |n| outgoing(n) }.flatten(1)
+
+      unsorted_graph = Graph.new(edges - removed_edges, nodes - start_nodes)
+
+      start_nodes + unsorted_graph.topological_sort
+    end
+  end
+
+  def outgoing(node)
+    edges.select { |e| node == from(e) }
+  end
+
+  def incoming(node)
+    edges.select { |e| node == to(e) }
+  end
+
+  def next_nodes(node)
+    outgoing(node).map { |e| to(e) }
+  end
+
+  def previous_nodes(node)
+    incoming(node).map { |e| from(e) }
+  end
+
+  private
+
+  def from(edge)
+    edge.first
+  end
+
+  def to(edge)
+    edge.last
+  end
+end
---
+++ graph_spec.rb
@@ -0,0 +1,56 @@
+require 'rspec/autorun'
+load 'graph.rb'
+
+describe Graph do
+  describe '#outgoing' do
+    it 'returns the outgoing edges' do
+      g = Graph.new([[1, 2], [2, 3], [3, 4], [2, 5]])
+
+      expect(g.outgoing(2)).to include [2, 3], [2, 5]
+    end
+  end
+
+  describe '#incoming' do
+    it 'returns the incoming edges' do
+      g = Graph.new([[1, 2], [2, 3], [3, 4], [2, 5], [5, 2]])
+
+      expect(g.incoming(2)).to include [1, 2], [5, 2]
+    end
+  end
+
+  describe '#next_nodes' do
+    it 'returns the next nodes' do
+      g = Graph.new([[1, 2], [2, 3], [3, 4], [2, 5]])
+
+      expect(g.next_nodes(2)).to include 3, 5
+    end
+  end
+
+  describe '#previous_nodes' do
+    it 'returns the previous nodes' do
+      g = Graph.new([[1, 2], [2, 3], [3, 4], [2, 5], [5, 2]])
+
+      expect(g.previous_nodes(2)).to include 1, 5
+    end
+  end
+
+  describe '#topological_sort' do
+    it 'topologically sorts the nodes of the graph' do
+      g = Graph.new([
+        [3, 8],
+        [3, 10],
+        [5, 11],
+        [7, 8],
+        [7, 11],
+        [8, 9],
+        [11, 2],
+        [11, 9],
+        [11, 10]
+      ])
+
+      expected = [3, 5, 7, 8, 11, 10, 9, 2]
+
+      expect(g.topological_sort).to eq(expected)
+    end
+  end
+end
