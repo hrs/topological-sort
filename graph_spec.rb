1c162e0c30ee9528d20f7eff56636afbf08b8131
Harry Schwartz <hello@harryrschwartz.com>
2014-08-16 10:07:13 -0400

    Switch to double quotes

--- graph_spec.rb
+++ graph_spec.rb
@@ -1,41 +1,41 @@
-require 'rspec/autorun'
-load 'graph.rb'
+require "rspec/autorun"
+load "graph.rb"
 
 describe Graph do
-  describe '#outgoing' do
-    it 'returns the outgoing edges' do
+  describe "#outgoing" do
+    it "returns the outgoing edges" do
       g = Graph.new([[1, 2], [2, 3], [3, 4], [2, 5]])
 
       expect(g.outgoing(2)).to include [2, 3], [2, 5]
     end
   end
 
-  describe '#incoming' do
-    it 'returns the incoming edges' do
+  describe "#incoming" do
+    it "returns the incoming edges" do
       g = Graph.new([[1, 2], [2, 3], [3, 4], [2, 5], [5, 2]])
 
       expect(g.incoming(2)).to include [1, 2], [5, 2]
     end
   end
 
-  describe '#next_nodes' do
-    it 'returns the next nodes' do
+  describe "#next_nodes" do
+    it "returns the next nodes" do
       g = Graph.new([[1, 2], [2, 3], [3, 4], [2, 5]])
 
       expect(g.next_nodes(2)).to include 3, 5
     end
   end
 
-  describe '#previous_nodes' do
-    it 'returns the previous nodes' do
+  describe "#previous_nodes" do
+    it "returns the previous nodes" do
       g = Graph.new([[1, 2], [2, 3], [3, 4], [2, 5], [5, 2]])
 
       expect(g.previous_nodes(2)).to include 1, 5
     end
   end
 
-  describe '#topological_sort' do
-    it 'topologically sorts the nodes of the graph' do
+  describe "#topological_sort" do
+    it "topologically sorts the nodes of the graph" do
       g = Graph.new([
         [3, 8],
         [3, 10],
